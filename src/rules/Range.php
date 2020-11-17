f36eaad0ccff0b5d1f2bc06205d842475dcf8898
Andrey <inbox.shelamkoff@gmail.com>
2020-11-17 17:48:53 +0300

    Update Range.php

--- src/rules/Range.php
+++ src/rules/Range.php
@@ -1,15 +1,12 @@
 <?php
 
 
-namespace App\Validator\Rules;
-
-
-use App\Validator\RuleInterface;
+namespace Bermuda\Validation\Rules;
 
 
 /**
  * Class Range
- * @package App\Chain\Rules
+ * @package Bermuda\Validation\Rules
  */
 final class Range implements RuleInterface
 {
