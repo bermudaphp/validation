6fc79dfe09c0d3f40cc59dc63d4b36ad933515fc
Andrey <inbox.shelamkoff@gmail.com>
2020-11-17 18:42:56 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,18 +1,14 @@
 <?php
 
 
+namespace Bermuda\Validation;
+
+
 /**
  * @param array $data
  * @return Validator
  */
 function v(array $data = []): Validator
 {
-    $v = new Validator();
-    
-    foreach ($data as $name => $datum)
-    {
-        $v->add($name, $datum[0], $datum[1] ?? false);
-    }
-    
-    return $v;
+    return Validator::make($data);
 }
