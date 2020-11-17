5179c5d43b53079b19ac789e9b7985cc4e85b7f1
Andrey <inbox.shelamkoff@gmail.com>
2020-11-17 17:41:09 +0300

    Create functions.php

---
+++ src/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * @param array $data
+ * @return Validator
+ */
+function v(array $data = []): Validator
+{
+    $v = new Validator();
+    
+    foreach ($data as $name => $datum)
+    {
+        $v->add($name, $datum[0], $datum[1]);
+    }
+    
+    return $v;
+}
