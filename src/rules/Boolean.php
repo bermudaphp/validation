2061ad2ae24a5fbbe11b24f42bf1a58c69dfc0b0
Andrey <inbox.shelamkoff@gmail.com>
2020-12-03 19:19:26 +0300

    Update and rename IsBool.php to Boolean.php

---
+++ src/rules/Boolean.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+
+use function Bermuda\str_equals;
+
+
+/**
+ * Class Boolean
+ * @package Bermuda\Validation\Rules
+ */
+class Boolean implements RuleInterface
+{
+    use RuleTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke($value): array
+    {
+        return is_bool($value) || (is_numeric($value) && ($value == 1 || $value == 0)) 
+            || (is_string($value) && str_equals_any($value, ['on', 'off'], true)) 
+            ? $this->validateNext($value) : 
+        ['The value must be boolean or equal to any of: 1, 0, on, off!'];
+    }
+}
