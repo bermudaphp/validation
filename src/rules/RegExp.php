7d55c99caba5338599038e676e81b0ec57368058
Andrey <inbox.shelamkoff@gmail.com>
2020-12-04 18:41:50 +0300

    Create RegExp.php

---
+++ src/rules/RegExp.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+
+use Bermuda\String\Str;
+
+
+/**
+ * Class YoutubeVideo
+ * @package Bermuda\Validation\Rules
+ */
+class RegExp implements RuleInterface
+{
+    private string $exp;
+
+    public function __construct(string $exp)
+    {
+        $this->exp = $exp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExp(): string
+    {
+        return $this->exp;
+    }
+
+    /**
+     * @param $value
+     * @return array
+     */
+    public function __invoke($value): array
+    {
+        if (Str::match($this->exp, (string) $value))
+        {
+            return $this->validateNext($value);
+        }
+
+        return [sprintf('The value must match the regular expression: %s', $this->exp)];
+    }
+}
