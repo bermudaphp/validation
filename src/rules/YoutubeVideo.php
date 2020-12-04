4b0bea939739979ebc495442d2a7963f2bbfaa35
Andrey <inbox.shelamkoff@gmail.com>
2020-12-04 18:42:57 +0300

    Update YoutubeVideo.php

--- src/rules/YoutubeVideo.php
+++ src/rules/YoutubeVideo.php
@@ -10,23 +10,12 @@ use Bermuda\String\Str;
  * Class YoutubeVideo
  * @package Bermuda\Validation\Rules
  */
-class YoutubeVideo implements RuleInterface
+final class YoutubeVideo extends RegExp
 {
-    use RuleTrait;
-
     public const regexp = '~^(?:https?://)?(?:www[.])?(?:youtube[.]com/watch[?]v=|youtu[.]be/)([^&]{11})~x';
 
-    /**
-     * @param $value
-     * @return array
-     */
-    public function __invoke($value): array
+    public function __construct()
     {
-        if (Str::match(self::regexp, (string) $value))
-        {
-            return $this->validateNext($value);
-        }
-
-        return [sprintf('The value must match the regular expression: %s', self::regexp)];
+        parent::__construct(self::regexp);
     }
 }
