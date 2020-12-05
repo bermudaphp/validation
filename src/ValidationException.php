4c6a68e958c01adb03b1da17ffb6e79780f55a21
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-05 13:45:09 +0300

    Update ValidationException.php

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Bermuda\Validation;
 
 
@@ -12,17 +11,29 @@ class ValidationException extends \RuntimeException
 {
     protected array $errors = [];
 
-    public function __construct(array $errors)
+    public function __construct(array $stack, array $errors)
     {
+        $this->file = $stack['file'];
+        $this->line = $stack['line'];
+
         $this->errors = $errors;
-        parent::__construct('Validation is failed!');
+        parent::__construct($this->stackToSring($stack));
     }
-    
+
     /**
      * @return array
      */
     public function getErrors(): array
     {
         return $this->errors;
+    }
+    
+    /**
+     * @param array $stack
+     * @return string
+     */
+    private function stackToSring(array $stack): string
+    {
+        return $stack['class'] . '::' . $stack['function'] . ' failed validation!';
     }
 }
