b253b8806a93d8e3a4aa48349897c27185983f95
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-05 14:22:57 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,9 @@
 <?php
 
-
 namespace Bermuda\Validation;
+
+
+use Bermuda\Validation\Rules\RuleInterface;
 
 
 /**
@@ -11,4 +13,22 @@ namespace Bermuda\Validation;
 function v(array $data = []): Validator
 {
     return Validator::make($data);
+}
+
+/**
+ * @param RuleInterface $rule
+ * @return void
+ * @throws ValidationException
+ */
+function validate(RuleInterface $rule, $value): void
+{
+    $errors = $rule($value);
+    
+    if ($errors != [])
+    {
+        $previous = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
+        $previous['class'] = get_class($rule);
+
+        throw new ValidationException($previous, $errors);
+    }
 }
