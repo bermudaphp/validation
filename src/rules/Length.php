faa441658a630ed126990a8cfa53bdc755327110
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-13 21:18:50 +0200

    Update Length.php

--- src/rules/Length.php
+++ src/rules/Length.php
@@ -7,34 +7,131 @@ namespace Bermuda\Validation\Rules;
  * Class Length
  * @package Bermuda\Validation\Rules
  */
-final class Length implements RuleInterface
+class StrLength extends AbstractRule
 {
-    use RuleTrait;
+    protected int $length;
+    protected bool $multibyte;
 
-    private bool $mb;
-    private int $value;
-
-    public function __construct(int $value, bool $mb = true)
+    public function __construct(int $length, bool $multibyte = true)
     {
-        $this->mb = $mb;
-        $this->value = $value;
+        $this->length = $length;
+        $this->multibyte = $multibyte;
+    }
+
+    protected function validate($value): bool
+    {
+        return is_string($value) && $this->getStringLen($value) == $this->length;
+    }
+
+    protected function getMessageFor($value): array
+    {
+        ['Length must be equal to ' . $this->value];
     }
 
     /**
-     * @param $value
-     * @return array
+     * @param int $length
+     * @param bool $multibite
+     * @return static
      */
-    public function __invoke($value): array
+    public static function equals(int $length, bool $multibyte = true): self
     {
-        return is_string($value) && $this->getStringLen($value) == $this->value
-            ? $this->validateNext($value) : ['String length must be equal to ' . $this->value];
+        return new self($length, $multibyte);
+    }
+
+    /**
+     * @param int $length
+     * @param bool $multibite
+     * @return static
+     */
+    public static function greaterThan(int $length, bool $multibyte = true): self
+    {
+        return new class($length, $multibyte) extends StrLength
+        {
+            protected function validate($value): bool
+            {
+                return is_string($value) && $this->getStringLen($value) > $this->length;
+            }
+        };
+    }
+
+    /**
+     * @param int $length
+     * @param bool $multibite
+     * @return static
+     */
+    public static function greaterThanOrEquals(int $length, bool $multibyte = true): self
+    {
+        return new class($length, $multibyte) extends StrLength
+        {
+            protected function validate($value): bool
+            {
+                return is_string($value) && $this->getStringLen($value) >= $this->length;
+            }
+        };
+    }
+
+    /**
+     * @param int $length
+     * @param bool $multibite
+     * @return static
+     */
+    public static function lessThan(int $length, bool $multibyte = true): self
+    {
+        return new class($length, $multibyte) extends StrLength
+        {
+            protected function validate($value): bool
+            {
+                return is_string($value) && $this->getStringLen($value) < $this->length;
+            }
+        };
+    }
+
+    /**
+     * @param int $length
+     * @param bool $multibite
+     * @return static
+     */
+    public static function lessThanOrEquals(int $length, bool $multibyte = true): self
+    {
+        return new class($length, $multibyte) extends StrLength
+        {
+            protected function validate($value): bool
+            {
+                return is_string($value) && $this->getStringLen($value) <= $this->length;
+            }
+        };
+    }
+
+    /**
+     * @param int $length
+     * @param bool $multibite
+     * @return static
+     */
+    public static function range(int $left, int $right, bool $multibyte = true): self
+    {
+        return new class($left, $right, $multibyte) extends StrLength
+        {
+            private int $right;
+
+            public function __construct(int $length, int $right, bool $multibyte = true)
+            {
+                parent::__construct($length, $right, $multibyte);
+            }
+
+            protected function validate($value): bool
+            {
+                return is_string($value) && $this->length
+                    <= ($len = $this->getStringLen($value))
+                    && $this->right >= $len;
+            }
+        };
     }
 
     /**
      * @param string $len
      * @return int
      */
-    private function getStringLen(string $value): int
+    protected function getStringLen(string $value): int
     {
         return $this->mb ? mb_strlen($value) : strlen($value);
     }
