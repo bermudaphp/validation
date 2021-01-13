3c994e3d6c1cf7c99de82c01ad4b926631dcafbc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-13 13:41:35 +0200

    Create Url.php

---
+++ src/rules/Url.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+
+use Bermuda\String\Str;
+
+
+/**
+ * Class Url
+ * @package Bermuda\Validation\Rules
+ */
+final class Url extends RegExp
+{
+    public const regexp ='%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu';
+    public function __construct()
+    {
+        parent::__construct(self::regexp);
+    }
+
+    /**
+     * @return string
+     */
+    public function getExp(): string
+    {
+        return $this->exp;
+    }
+
+    /**
+     * @param string $regExp
+     * @return $this
+     */
+    public function withExp(string $regExp): self
+    {
+        throw new \RuntimeException(__METHOD__ . ' is dissable from this class');
+    }
+    
+    protected function getMessageFor($value): array
+    {
+        return ['Value must be a correct url'];
+    }
+}
