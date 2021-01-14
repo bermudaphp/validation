557838972a4c4faaca1dc0a58e319ff6aa3180f5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-15 01:03:41 +0200

    Update Range.php

--- src/rules/Range.php
+++ src/rules/Range.php
@@ -2,93 +2,39 @@
 
 namespace Bermuda\Validation\Rules;
 
-
 /**
  * Class Range
  * @package Bermuda\Validation\Rules
  */
-class Range extends AbstractRule
+final class Range extends AbstractRule
 {
-    use RuleTrait;
+    private $x, $y;
+    private string $dateTimeFormat;
 
-    protected $x, $y;
-
-    public function __construct($x, $y)
+    public function __construct($x, $y, string $dateTimeFormat = 'd/m/Y')
     {
-        $this->x = $x;
-        $this->y = $y;
+        $this->x = $x; $this->y = $y;
+        $this->dateTimeFormat = $dateTimeFormat;
     }
 
     /**
-     * @param $value
-     * @return bool
+     * @inheritDoc
      */
-    protected function validate($value): bool
+    protected function validate(&$value): bool
     {
-        return is_numeric($value) && $value >= $this->x && $value <= $this->y;
+        return $value >= $this->x && $value <= $this->y;
     }
     
-    protected function getMessageFor($value): array
-    {
-        return ["Must be a number in the range from {$this->x} to {$this->y}"];
-    }
-
     /**
-     * @param string|\DateTimeInterface $x
-     * @param string|\DateTimeInterface $y
-     * @return DateTimeFactoryAwareTrait|self
-     * @throws \InvalidArgumentException
+     * @inheritDoc
      */
-    public static function date($x, $y): self
+    protected function getMessageFor($value): string
     {
-        self::check('x', $x);
-        self::check('y', $y);
-        
-        return new class($x, $y) extends Range
+        if ($value instanceof \DateTimeInterface)
         {
-            use DateTimeFactoryAwareTrait;
-            
-            /**
-             * @var \DateTimeInterface
-             */
-            protected $x, $y;
-
-            public function __construct(\DateTimeInterface $x, \DateTimeInterface $y)
-            {
-                $this->x = $x;
-                $this->y = $y;
-            }
-            
-            protected function validate($value): bool
-            {
-                if ($value instanceof \DateTimeInterface)
-                {
-                    $value = ($this->dateTimeFactory)($value);
-                }
-
-                return parent::validate($value);
-            }
-
-            protected function getMessageFor($value): array
-            {
-                return ["Must be a date in the range from {$this->x->format($this->datetimeFormat)} to {$this->y->format($this->datetimeFormat)}"];
-            }
-        };
-    }
-    
-    private static function check(string $name, &$argument): void
-    {
-        if (!$argument instanceof \DateTimeInterface)
-        {
-            try
-            {
-                $argument = new \DateTime($argument);
-            }
-
-            catch (\Throwable $e)
-            {
-                throw new \InvalidArgumentException("{$name} must be a \DateTimeInterface instance or datetime string", 0, $e);
-            }
+            return "Must be a date in the range from {$this->x->format($this->datetimeFormat)} to {$this->y->format($this->datetimeFormat)}";
         }
+
+        return "Must be a number in the range from {$this->x} to {$this->y}"];
     }
 }
