1d68ca455fd98a7e0b1c297194e1a02b521d9518
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-14 23:14:01 +0200

    Update LessThanEquals.php

--- src/rules/LessThanEquals.php
+++ src/rules/LessThanEquals.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace Bermuda\Validation\Rules;
 
 /**
  * Class LessThanEquals
- * @package App
+ * @package Bermuda\Validation\Rules
  */
 class LessThanEquals extends LessThan
 {
@@ -20,26 +20,29 @@ class LessThanEquals extends LessThan
 
     /**
      * @param string|\DateTimeInterface $operand
+     * @param string $format
      * @return DateTimeFactoryAwareTrait|self
      * @throws \InvalidArgumentException
      */
-    public static function date($operand = 'now'): self
+    public static function date($operand = 'now', string $format = 'd/m/Y'): self
     {
         static::check($operand);
         return new class($operand) extends GreaterThanEquals
         {
             use DateTimeFactoryAwareTrait;
 
-            public function __construct(\DateTimeInterface $operand)
+            public function __construct(\DateTimeInterface $operand, string $format)
             {
                 $this->operand = $operand;
+                $this->datetimeFormat = $format;
+                $this->getDatetimeFactory();
             }
 
             protected function validate($value): bool
             {
                 if ($value instanceof \DateTimeInterface)
                 {
-                    $value = ($this->dateTimeFactory)($value);
+                    $value = ($this->dateTimeFactory)($value, $this->datetimeFormat);
                 }
 
                 return parent::validate($value);
