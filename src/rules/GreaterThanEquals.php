5605d332923c259226f1e8f2a85eab45628549f5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-14 23:06:32 +0200

    Update GreaterThanEquals.php

--- src/rules/GreaterThanEquals.php
+++ src/rules/GreaterThanEquals.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace Bermuda\Validation\Rules;
 
 /**
  * Class GreaterThanEquals
- * @package App
+ * @package Bermuda\Validation\Rules
  */
 class GreaterThanEquals extends GreaterThan
 {
@@ -23,23 +23,25 @@ class GreaterThanEquals extends GreaterThan
      * @return DateTimeFactoryAwareTrait|self
      * @throws \InvalidArgumentException
      */
-    public static function date($operand = 'now'): self
+    public static function date($operand = 'now', string $format = 'd/m/Y'): self
     {
         static::check($operand);
-        return new class($operand) extends GreaterThanEquals
+        return new class($operand, string $format) extends GreaterThanEquals
         {
             use DateTimeFactoryAwareTrait;
 
-            public function __construct(\DateTimeInterface $operand)
+            public function __construct(\DateTimeInterface $operand, string $format)
             {
                 $this->operand = $operand;
+                $this->datetimeFormat = $format
+                $this->dateTimeFactory = $this->getDatetimeFactory();
             }
 
             protected function validate($value): bool
             {
                 if ($value instanceof \DateTimeInterface)
                 {
-                    $value = ($this->dateTimeFactory)($value);
+                    $value = ($this->dateTimeFactory)($value, $this->datetimeFormat);
                 }
 
                 return parent::validate($value);
