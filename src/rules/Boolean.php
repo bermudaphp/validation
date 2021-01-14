030a5c5a3de633b2c1858efd159b2528c0381fe0
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-14 23:26:21 +0200

    Update Boolean.php

--- src/rules/Boolean.php
+++ src/rules/Boolean.php
@@ -10,18 +10,22 @@ use function Bermuda\str_equals_any;
  * Class Boolean
  * @package Bermuda\Validation\Rules
  */
-class Boolean implements RuleInterface
+final class Boolean extends AbstractRule
 {
-    use RuleTrait;
-
     /**
      * @inheritDoc
      */
-    public function __invoke($value): array
+    protected function validate($value): bool
     {
         return is_bool($value) || (is_numeric($value) && ($value == 1 || $value == 0)) 
-            || (is_string($value) && str_equals_any($value, ['on', 'off'], true)) 
-            ? $this->validateNext($value) : 
-        ['The value must be boolean or equal to any of: 1, 0, on, off!'];
+            || (is_string($value) && str_equals_any($value, ['on', 'off'], true));
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    protected function getMessageFor($value): array
+    {
+        ['Must be boolean or equal to any of: 1, 0, on, off!'];
     }
 }
