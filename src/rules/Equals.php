923d68c809ca5ea1ad32bb175df8b989caf1a07f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-15 00:51:24 +0200

    Update Equals.php

--- src/rules/Equals.php
+++ src/rules/Equals.php
@@ -2,129 +2,44 @@
 
 namespace Bermuda\Validation\Rules;
 
-
-use function Bermuda\str_equals;
-
+use Bermuda\String\Str;
 
 /**
  * Class Equals
  * @package Bermuda\Validation\Rules
  */
-abstract class Equals implements RuleInterface
+class Equals extends AbstractRule
 {
-    use RuleTrait
-    {
-        validateNext as protected;
-    }
-
     /**
-     * @var mixed
+     * @var float|int|string|\DateTimeInterface
      */
     protected $operand;
+    protected string $dateTimeFormat;
 
-    /**
-     * Equals constructor.
-     * @param int|string|\DateTimeInterface
-     */
-    protected function __construct($operand)
+    protected function __construct($operand, string $dateTimeFormat = 'd/m/Y')
     {
         $this->operand = $operand;
+        $this->dateTimeFormat = $dateTimeFormat;
     }
-
+    
     /**
-     * @param string $operand
-     * @param bool $caseInsensitive
-     * @return static
+     * @inheritDoc
      */
-    public static function string(string $operand, bool $caseInsensitive = false): self
+    protected function validate(&$value): bool
     {
-        return new class($operand, $caseInsensitive) extends Equals
-        {
-            private bool $caseInsensitive;
-
-            public function __construct(string $operand, bool $caseInsensitive)
-            {
-                parent::__construct($operand);
-                $this->caseInsensitive = $caseInsensitive;
-            }
-
-            /**
-             * @param $v
-             * @return array
-             */
-            public function validate($v): array
-            {
-                $v = (string) $v;
-
-                if (str_equals($v, $this->operand, $this->caseInsensitive))
-                {
-                    return $this->validateNext($v);
-                }
-
-                return ['The value must be a string and equal to ' . $this->operand];
-            }
-        };
+        return $value == $this->operand
     }
-
+    
     /**
-     * @param $operand
-     * @return static
+     * @inheritDoc
      */
-    public static function number($operand): self
+    protected function getMessageFor($value): string
     {
-        if (!is_numeric($operand))
+        if ($this->operand instanceof \DateTimeInterface)
         {
-            throw new \InvalidArgumentException('Operand must be a number');
+            return 'Must be date and equals to' $this->operand->format($this->dateTimeFormat);
         }
-
-        return new class($operand) extends Equals
-        {
-            /**
-             * @param $v
-             * @return array
-             */
-            public function validate($v): array
-            {
-                return $this->operand == $v ? $this->validateNext($v) : ['The value must be a number and equal to '. $this->operand];
-            }
-        };
-    }
-
-    /**
-     * @param \DateTimeInterface $operand
-     * @param string $format
-     * @return static
-     */
-    public static function date(\DateTimeInterface $operand, string $format = 'Y-m-d H:i:s'): self
-    {
-        return new class($operand, $format) extends Equals
-        {
-            private string $format;
-
-            public function __construct(\DateTimeInterface $operand, string $format)
-            {
-                $this->format = $format;
-                parent::__construct($operand);
-            }
-
-            /**
-             * @param \DateTimeInterface|string $v
-             * @return array
-             */
-            public function validate($v): array
-            {
-                if ($v !instanceof \DateTimeInterface)
-                {
-                    $v = new \DateTime($v);
-                }
-                
-                return $this->operand->format($this->format) == $v->format($this->format)
-                    ? $this->validateNext(null, $v) :
-                    [
-                        'The value must be equal to '.
-                        $this->operand->format($this->format)
-                    ];
-            }
-        };
+        
+        return 'Must be equals to ' . $this->operand;
     }
 }
