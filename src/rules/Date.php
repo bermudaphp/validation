c4e8692803f7b77eec67a039a4abe06a4c3d8922
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-15 13:14:25 +0200

    Update Date.php

--- src/rules/Date.php
+++ src/rules/Date.php
@@ -35,7 +35,7 @@ final class Date extends AbstractRule
      * @param string $format
      * @return self
      */
-    public static function equals(\DateTimeInterface $operand, string $format = 'd/m/Y'): self
+    public static function equals(\DateTimeInterface $operand, string $format = 'd/m/Y'): RuleInterface
     {
         return (new self($format))->setNext(new Equals($operand));
     }
@@ -45,7 +45,7 @@ final class Date extends AbstractRule
      * @param string $format
      * @return self
      */
-    public static function greaterThenEquals(\DateTimeInterface $operand, string $format = 'd/m/Y'): self
+    public static function greaterThenEquals(\DateTimeInterface $operand, string $format = 'd/m/Y'): RuleInterface
     {
         return (new self($format))->setNext(new GreaterThenEquals($operand));
     }
@@ -55,7 +55,7 @@ final class Date extends AbstractRule
      * @param string $format
      * @return self
      */
-    public static function greaterThen(\DateTimeInterface $operand, string $format = 'd/m/Y'): self
+    public static function greaterThen(\DateTimeInterface $operand, string $format = 'd/m/Y'): RuleInterface
     {
         return (new self($format))->setNext(new GreaterThen($operand));
     }
@@ -65,7 +65,7 @@ final class Date extends AbstractRule
      * @param string $format
      * @return self
      */
-    public static function lessThen(\DateTimeInterface $operand, string $format = 'd/m/Y'): self
+    public static function lessThen(\DateTimeInterface $operand, string $format = 'd/m/Y'): RuleInterface
     {
         return (new self($format))->setNext(new LessThen($operand));
     }
@@ -75,7 +75,7 @@ final class Date extends AbstractRule
      * @param string $format
      * @return self
      */
-    public static function lessThenEquals(\DateTimeInterface $operand, string $format = 'd/m/Y'): self
+    public static function lessThenEquals(\DateTimeInterface $operand, string $format = 'd/m/Y'): RuleInterface
     {
         return (new self($format))->setNext(new LessThenEquals($operand));
     }
@@ -83,7 +83,7 @@ final class Date extends AbstractRule
     /**
      * @inheritDoc
      */
-    protected function getMessageFor($value): string
+    protected function getMessageFor($value): RuleInterface
     {
         return 'Must be a date';
     }
