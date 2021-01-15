df35f992a5c7619ed3956b856d492442d3f6020c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-15 02:31:36 +0200

    Update File.php

--- src/rules/File.php
+++ src/rules/File.php
@@ -3,17 +3,39 @@
 namespace Bermuda\Validation\Rules;
 
 /**
- * Class UploadedFile
+ * Class File
  * @package Bermuda\Validation\Rules
  */
 class File extends AbstractRule
 {
+    protected ?int $maxImageSize = null;
+
+    public function __construct(?int $maxFileSize = null)
+    {
+        $this->maxFileSize = $maxImageSize;
+    }
     /**
      * @inheritDoc
      */
     protected function validate(&$value): bool
     {
-        return is_uploaded_file($value);
+        if (is_uploaded_file($value))
+        {
+            return $this->checkMaxFileSize($value, sprintf('File size must be less than %s b', $this->maxFileSize));
+        }
+        
+        return false;
+    }
+    
+    protected function checkMaxFileSize($value, string $msg): bool
+    {
+        if ($this->maxFileSize < filesize($value))
+        {
+            $this->msg = $msg;
+            return false;
+        }
+        
+        return true;
     }
     
     /**
