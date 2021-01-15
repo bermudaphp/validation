f8f8a91a087cdc9a2a90cc282060ca21b4de7c7a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-15 13:20:25 +0200

    Create DateTimeFactoryAwareTrait.php

---
+++ src/rules/DateTimeFactoryAwareTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+/**
+ * Trait DateTimeFactoryAwareTrait
+ * @package Bermuda\Validation\Rules
+ */
+trait DateTimeFactoryAwareTrait
+{
+    private $dateTimeFactory = null;
+    private string $datetimeFormat = 'd/m/Y';
+
+    /**
+     * @param callable $factory
+     */
+    public function setDatetimeFactory(callable $factory): void
+    {
+        $this->dateTimeFactory = static function($v, ?string $format = null) use ($factory): \DateTimeInterface
+        {
+            return $factory($v, $format);
+        };
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateTimeFactory(): callable
+    {
+        if ($this->dateTimeFactory == null)
+        {
+            return static function($v, ?string $format = null): \DateTime
+            {
+                return \DateTime::createFromFormat($format, $v);
+            };
+        }
+
+        return $this->dateTimeFactory;
+    }
+
+    /**
+     * @param string $format
+     */
+    public function setDatetimeFormat(string $format): void
+    {
+        $this->datetimeFormat = $format;
+    }
+}
