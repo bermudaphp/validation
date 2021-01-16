401bcad95670cbb045c65af34721614e1715b1d4
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-01-16 20:59:01 +0200

    Update AllowEmpty.php

--- src/rules/AllowEmpty.php
+++ src/rules/AllowEmpty.php
@@ -6,11 +6,11 @@ namespace Bermuda\Validation\Rules;
  * Class AllowEmpty
  * @package Bermuda\Validation\Rules
  */
-final class AllowEmpty implements RuleInterface;
+final class AllowEmpty implements RuleInterface
 {
     use RuleTrait;
     
-    public function __construct(RuleInterface $next)
+    public function __construct(?RuleInterface $next = null)
     {
         $this->setNext($next);
     }
@@ -22,5 +22,32 @@ final class AllowEmpty implements RuleInterface;
     public function __invoke($value): array
     {
         return !empty($value) ? $this->validateNext($value) : [];
+    }
+
+    /**
+     * @param RuleInterface|null $rule
+     * @return static
+     */
+    public static function make(?RuleInterface $rule = null): self
+    {
+        return new self($rule);
+    }
+
+    /**
+     * @param RuleInterface|RuleInterface[] $rule
+     * @return AllOf
+     */
+    public static function allOf($rule): self
+    {
+        return new self(AllOf::make($rule));
+    }
+
+    /**
+     * @param RuleInterface|RuleInterface[] $rule
+     * @return OneOf
+     */
+    public static function oneOf($rule): self
+    {
+        return new self(OneOf::make($rule));
     }
 }
