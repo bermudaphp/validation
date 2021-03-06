fcf2866b31bc321adcd1db399b36967b3407cb5d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-06 15:17:50 +0300

    Update AbstractRule.php

--- src/rules/AbstractRule.php
+++ src/rules/AbstractRule.php
@@ -2,6 +2,8 @@
 
 namespace Bermuda\Validation\Rules;
 
+use Bermuda\String\Str;
+
 /**
  * Class AbstractRule
  * @package Bermuda\Validation\Rules
@@ -10,12 +12,12 @@ abstract class AbstractRule implements RuleInterface
 {
     use RuleTrait ;
     protected string $msg = '';
-    
+
     public function __construct(string $msg = '')
     {
         $this->msg = $msg;
     }
-    
+
     /**
      * @param $value
      * @return array
@@ -32,29 +34,60 @@ abstract class AbstractRule implements RuleInterface
             $result = false;
         }
         
-        return  $result ? $this->validateNext($value) : $this->getMessage($value);
+        return  $result ? $this->validateNext($value) : [$this->getMessage($value)];
     }
     
     /**
-     * @param $msg
+     * @param $message
      * @return $this
      */
-    public function setMessage(string $msg): self
+    public function setMessage(string $message): self
     {
-        $this->msg = $msg;
+        $this->msg = $message;
         return $this;
     }
     
-    protected function getMessage($value): array
+    protected function getMessage($value):string
     {
-        if ($this->msg == '')
+        if ($this->needReplacement())
         {
-            return [$this->getMessageFor($value)];
+           return $this->replace($value); 
         }
-        
-        return [$this->msg];
+
+        return $this->msg;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getReplacmentAttributes(): array
+    {
+        return [];
+    }
+
+    protected function needReplacement(): bool
+    {
+        return false;
+    }
+
+    protected function replace($value)
+    {
+        $search = [];
+        $replacment = [];
+
+        if (($replacment != $this->getReplacmentAttributes()) != [])
+        {
+            $replacment = $this->getReplacmentAttributes();
+
+            $search = array_keys($replacment);
+            $replacment = array_values($replacment);
+        }
+
+        $search[] = ':v';
+        $replacment[] = $value;
+
+        return Str::replace($this->msg, $search, $replacment);
     }
      
     abstract protected function validate(&$value): bool ;
-    abstract protected function getMessageFor($value): string ;
 }
