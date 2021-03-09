e41729386e872b0483f4378cf1227813538da9c3
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-09 15:15:27 +0300

    Update Date.php

--- src/rules/Date.php
+++ src/rules/Date.php
@@ -82,7 +82,7 @@ final class Date extends AbstractRule
     /**
      * @inheritDoc
      */
-    protected function getMessage($value): string
+    protected function getDefaultMessage(): string
     {
         return 'Must be a date';
     }
