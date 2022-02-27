8ba36d12fc9a002a8ac581ff0ebd2588f8e64ae0
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 00:10:12 +0200

    Update OneOf.php

--- src/rules/OneOf.php
+++ src/rules/OneOf.php
@@ -4,80 +4,19 @@ namespace Bermuda\Validation\Rules;
 
 use Generator;
 
-class OneOf implements RuleInterface, \IteratorAggregate
+final class OneOf implements RuleCollectionInterface
 {
-    use RuleTrait;
-    /**
-     * @var RuleInterface[]
-     */
-    protected array $rules = [];
-    public function __construct(array $rules = [])
-    {
-        if ($rules === []) {
-            throw new \InvalidArgumentException('Rule array must not be empty');
-        }
-
-        $this->addRules($rules);
-    }
-
-    protected function doValidate($var): bool
-    {
-        return true;
-    }
+    use RuleCollectionTrait;
 
     /**
-     * @return RuleInterface[]
-     */
-    final public function getIterator(): Generator
-    {
-        foreach ($this->rules as $rule) yield $rule;
-    }
-
-    /**
-     * @return RuleInterface[]
-     */
-    public function getRules(): array
-    {
-        return $this->rules;
-    }
-
-    /**
-     * @param string $name
-     * @return bool
-     */
-    final public function ruleExists(string $name): bool
-    {
-        return isset($this->rules[$name]);
-    }
-
-    /**
-     * @param RuleInterface $rule
-     * @return $this
-     */
-    final public function addRule(RuleInterface $rule): self
-    {
-        $this->rules[$rule::class] = $rule;
-        return $this;
-    }
-
-    /**
-     * @param RuleInterface[] $rules
-     * @return $this
-     */
-    final public function addRules(iterable $rules): self
-    {
-        foreach ($rules as $rule) $this->addRule($rule);
-        return $this;
-    }
-
-    /**
-     * @inerhitDoc
+     * @param $value
+     * @return bool|string
      */
     public function validate($value): bool|string
     {
         foreach ($this->rules as $rule) {
             if (($result = $rule->validate($value)) === true) {
-                return $this->validateNext($value);
+                return true;
             }
         }
 
