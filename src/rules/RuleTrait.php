1e4800ae488e8896d3b1639f5ae934aa1ca4beae
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 23:38:07 +0200

    Update RuleTrait.php

--- src/rules/RuleTrait.php
+++ src/rules/RuleTrait.php
@@ -32,6 +32,8 @@ trait RuleTrait
 
     protected function getWildcards($var): array
     {
+        $wildcards = [];
+        
         if (strpos($this->message, $this->valueWildcard) !== false) {
             $wildcards = [$this->valueWildcard => $this->prepareVar($var)];
         }
@@ -40,7 +42,7 @@ trait RuleTrait
             $wildcards = array_merge($wildcards, $this->wildcards);
         }
 
-        return $wildcards ?? [];
+        return $wildcards;
     }
 
     protected function prepareVar($var): string
