ef78d9214b70f64378b23d1873ea87c842216796
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 00:10:56 +0200

    Create RuleCollectionInterface.php

---
+++ src/rules/RuleCollectionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+use Traversable;
+
+interface RuleCollectionInterface extends \IteratorAggregate
+{
+    public function validate($value): bool|string|array;
+    public function addRule(RuleInterface $rule): RuleCollectionInterface ;
+
+    /**
+     * @return Traversable<RuleInterface>
+     */
+    public function getIterator(): Traversable;
+}
