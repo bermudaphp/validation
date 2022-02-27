895ac7ec4d1d2a4e6564a932b600e15950def386
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 22:56:08 +0200

    Update RuleTrait.php

--- src/rules/RuleTrait.php
+++ src/rules/RuleTrait.php
@@ -2,39 +2,89 @@
 
 namespace Bermuda\Validation\Rules;
 
-/**
- * Trait RuleTrait
- * @package Bermuda\Validation\Rules
- */
 trait RuleTrait
 {
+    protected string $message = '';
+    protected ?array $wildcards = [];
+    protected string $valueWildcard = ':v';
     protected ?RuleInterface $next = null;
 
     /**
      * @param RuleInterface|null $rule
-     * @return $this
+     * @return RuleInterface
      */
     public function setNext(?RuleInterface $rule): RuleInterface
-    { 
-        $this->next != null && $rule != null ? 
-            $this->next->setNext($rule) : 
-            $this->next = $rule;
-        
+    {
+        $this->next = $rule;
+        return $rule;
+    }
+
+    /**
+     * @param string $pattern
+     * @return RuleInterface
+     */
+    public function setMessage(string $pattern): RuleInterface
+    {
+        $this->message = $pattern;
         return $this;
     }
+
+    /**
+     * @inerhitDoc
+     */
+    public function validate($var): bool|string
+    {
+        if ($this->checkType($var) && ($result = $this->doValidate($this->prepareVar($var))) === true) {
+            return $this->validateNext($var);
+        }
+
+        return $this->generateMessage($this->getWildcards($var));
+    }
+
+    protected function checkType($var): bool
+    {
+        return in_array(gettype($var), $this->getTypes());
+    }
+
+    protected function getTypes(): array
+    {
+        return ['string'];
+    }
+
+    protected function getWildcards($var): array
+    {
+        $wildcards = [$this->valueWildcard => $this->prepareVar($var)];
+
+        if ($this->wildcards !== []) {
+            $wildcards = array_merge($wildcards, $this->wildcards);
+        }
+
+        return $wildcards;
+    }
+
+    protected function prepareVar($var)
+    {
+        return $var;
+    }
+
+    abstract protected function doValidate($var): bool;
 
     /**
      * @param array $result
      * @param $value
      * @return array
      */
-    protected function validateNext($value): array
+    protected function validateNext($value): bool|string
     {
-        if ($this->next != null)
-        {
-            return ($this->next)($value);
-        }
+        return $this->next?->validate($value) ?? true;
+    }
 
-        return [];
+    /**
+     * @param array $wildcards
+     * @return string
+     */
+    protected function generateMessage(array $wildcards): string
+    {
+        return str_replace(array_keys($wildcards), $wildcards, $this->message);
     }
 }
