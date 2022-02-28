6ed97bb3d8eddb2ed208d6cb3d64db42218052c9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 19:50:26 +0200

    Update and rename Range.php to Between.php

---
+++ src/rules/Between.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+final class Between implements RuleInterface
+{
+    use RuleTrait;
+    public function __construct(int|float $min, int|float $max, string $message = 'Value must be between :min and :max')
+    {
+        $this->message = $message;
+        $this->wildcards[':min'] = $min;
+        $this->wildcards[':max'] = $max;
+    }
+
+    protected function doValidate($var): bool
+    {
+        return $this->wildcards[':min'] >= $var && $var <= $this->wildcards[':max'];
+    }
+}
