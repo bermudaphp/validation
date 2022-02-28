ee5f9868be58b65ccd6be17db82577a8dc780ca5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 21:22:33 +0200

    Update Date.php

--- src/rules/Date.php
+++ src/rules/Date.php
@@ -7,9 +7,10 @@ use Bermuda\Clock\Clock;
 class Date implements RuleInterface
 {
     use RuleTrait;
-    public function __construct(string $message = 'Value must be a valid date', protected ?string $format = null)
+    public function __construct(string $message = 'Value must be a valid date :format', protected ?string $format = null)
     {
         $this->message = $message;
+        $this->wildcards[':format'] = $format == null ? '' : ' in format: ' . $format;
     }
 
     protected function doValidate($var): bool
@@ -21,7 +22,7 @@ class Date implements RuleInterface
         }
     }
 
-    public static function between(\DateTimeInterface $min, \DateTimeInterface $max, string $message ='Value must be a valid date between :min and :max', ?string $format = null): self
+    public static function between(\DateTimeInterface $min, \DateTimeInterface $max, string $message ='Value must be a valid date :format between :min and :max', ?string $format = null): self
     {
         return new class($min, $max, $message, $format) extends Date {
             public function __construct(\DateTimeInterface $min, \DateTimeInterface $max, string $message, ?string $format)
