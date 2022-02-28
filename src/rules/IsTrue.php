a8615dd3dcfef1e0ebcb11a0dcdcedaa129a7468
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 19:12:11 +0200

    Create IsTrue.php

---
+++ src/rules/IsTrue.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+use Bermuda\String\StringHelper;
+
+final class IsFalse implements RuleInterface
+{
+    use RuleTrait;
+    public function __construct(string $message = '', private bool $strict = false)
+    {
+        if ($message == '') {
+            $message = $this->strict ? 'Value must be true'
+                : 'Value must be true or equal any of: \'true\', \'on\', \'1\'';
+        }
+
+        $this->message = $message;
+    }
+
+    protected function doValidate($var): bool
+    {
+        if ($this->strict) {
+            return $var === true;
+        }
+
+        return $var === true || (is_string($var) && StringHelper::equals($var, ['on', '1', 'true'])) || $var == 1;
+    }
+}
