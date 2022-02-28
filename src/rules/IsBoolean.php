a9838898f2aead32a485bda18ebe4fd4bc919cfc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-28 19:00:22 +0200

    Update and rename Boolean.php to IsBoolean.php

---
+++ src/rules/IsBoolean.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+use Bermuda\String\StringHelper;
+
+final class IsBoolean implements RuleInterface
+{
+    use RuleTrait;
+    public function __construct(string $message = '', private bool $strict = false)
+    {
+        if ($message == '') {
+            $message = $this->strict ? 'Value must be boolean' 
+                : 'Value must be boolean or string equal any of: on, off, true, false, 0, 1';
+        }
+        
+        $this->message = $message;
+    }
+
+    protected function doValidate($var): bool
+    {
+        if ($this->strict) {
+            return is_bool($var);
+        }
+        
+        return is_bool($var) || (is_string($var) && StringHelper::equals((string) $var, ['on', 'off', '1', '0', 'true', 'false']));
+    }
+}
