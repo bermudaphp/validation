6e2bd31631a2c2e839a2a0260162869645e3ce75
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 15:24:17 +0200

    Update Equals.php

--- src/rules/Equals.php
+++ src/rules/Equals.php
@@ -7,18 +7,14 @@ use Bermuda\Validation\Equalible;
 final class Equals implements RuleInterface
 {
     use RuleTrait;
-    public function __construct(private int|float|string|Equalible $operand, string $message = 'Value must be equal to :operand', private bool $strict = true)
+    public function __construct(private int|float|string $operand, string $message = 'Value must be equal to :operand', private bool $strict = true)
     {
         $this->message = $message;
         $this->wildcards[':operand'] = (string) $this->operand;
     }
-    
+
     protected function doValidate($var): bool
     {
-        if ($this->operand instanceof Equalible) {
-            return $this->operand->equalTo($var);
-        }
-        
         return $this->strict ? $var === $this->operand : $var == $this->operand;
     }
 }
