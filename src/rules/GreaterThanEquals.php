5b2e544e110fe604c2fc50ea6433a01d878f77e1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 14:54:28 +0200

    Update GreaterThanEquals.php

--- src/rules/GreaterThanEquals.php
+++ src/rules/GreaterThanEquals.php
@@ -2,51 +2,21 @@
 
 namespace Bermuda\Validation\Rules;
 
-/**
- * Class GreaterThanEquals
- * @package Bermuda\Validation\Rules
- */
-class GreaterThanEquals extends AbstractRule
+final class GreaterThanEquals implements RuleInterface
 {
-    /**
-     * @var float|int|string|\DateTimeInterface
-     */
-    protected $operand;
-    protected string $dateTimeFormat;
+    use RuleTrait;
+    public function __construct(float|int $operand, string $message = 'Value must be greater than or equal to :operand')
+    {
+        $this->message = $message;
+        $this->wildcards[':operand'] = $operand;
+    }
 
-    public function __construct($operand, string $dateTimeFormat = 'd/m/Y')
+    protected function doValidate($var): bool
     {
-        $this->operand = $operand;
-        $this->dateTimeFormat = $dateTimeFormat;
-        parent::__construct(null);
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    protected function validate(&$value): bool
-    {
-        return $value >= $this->operand;
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    protected function getDefaultMessage(): string
-    {
-        if ($this->operand instanceof \DateTimeInterface)
-        {
-            return 'Must be a date and greater than or equals :operand';
+        if (!is_numeric($var)) {
+            return false;
         }
-        
-        return 'Must be greater than or equals :operand';
-    }
-    
-    /**
-     * @return array
-     */
-    protected function getReplacmentAttributes(): array
-    {
-        return [':operand' => $this->operand instanceof \DateTimeInterface ? $this->operand->format($this->dateTimeFormat) : $this->operand];
+
+        return $var >= $this->wildcards[':operand'];
     }
 }
