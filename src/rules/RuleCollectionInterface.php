021184fd0f8b497d30d0c525523471cbd393abf5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 15:48:10 +0200

    Update RuleCollectionInterface.php

--- src/rules/RuleCollectionInterface.php
+++ src/rules/RuleCollectionInterface.php
@@ -8,7 +8,7 @@ interface RuleCollectionInterface extends \IteratorAggregate
 {
     public function validate($value): bool|string|array ;
     public function addRule(RuleInterface $rule): RuleCollectionInterface ;
-
+    public function hasRule(string|RuleInterface $rule): bool ;
     /**
      * @return Traversable<RuleInterface>
      */
