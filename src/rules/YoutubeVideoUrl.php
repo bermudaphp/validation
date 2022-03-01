619125ec80d453c1044b5a395f000316b5f5e114
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 15:18:25 +0200

    Update YoutubeVideoUrl.php

--- src/rules/YoutubeVideoUrl.php
+++ src/rules/YoutubeVideoUrl.php
@@ -7,7 +7,7 @@ final class YoutubeVideoUrl extends RegEx
     public const regEx = '~^(?:https?://)?(?:www[.])?(?:youtube[.]com/watch[?]v=|youtu[.]be/)([^&]{11})~x';
     public function __construct(string $message = 'Value must be youtube video url')
     {
-        parent::__construct(self::regexp, $message);
+        parent::__construct(self::regEx, $message);
     }
     
     /**
