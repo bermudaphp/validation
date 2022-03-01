6cbbe3a4440d239cc8cccc40cf389045aab6a4d8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 17:15:59 +0200

    Update RuleInterface.php

--- src/rules/RuleInterface.php
+++ src/rules/RuleInterface.php
@@ -4,15 +4,20 @@ namespace Bermuda\Validation\Rules;
 
 interface RuleInterface
 {
+    /**
+     * @return string return name of rule
+     */
     public function getName(): string ;
+
     /**
      * @param $value
      * @return bool|string
      * Returns true if the validation was successful otherwise returns an error message
      */
     public function validate($value): bool|string ;
-    
+
     /**
+     * Set rule validation error message
      * @return $this
      */
     public function setMessage(string $message): RuleInterface ;
