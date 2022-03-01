10ebfc5282ec76c585d994202c9b43b2f6a83619
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 17:18:31 +0200

    Update RuleCollectionInterface.php

--- src/rules/RuleCollectionInterface.php
+++ src/rules/RuleCollectionInterface.php
@@ -12,10 +12,10 @@ interface RuleCollectionInterface extends \IteratorAggregate
      * Returns true if the validation was successful otherwise returns an error message or array of errors
      */
     public function validate($value): bool|string|array ;
-    public function addRule(RuleInterface $rule): RuleCollectionInterface ;
-    public function hasRule(string|RuleInterface $rule): bool ;
+    public function addRule(RuleInterface|RuleCollectionInterface $rule): RuleCollectionInterface ;
+    public function hasRule(string|RuleInterface|RuleCollectionInterface $rule): bool ;
     /**
-     * @return Traversable<RuleInterface>
+     * @return Traversable<RuleInterface|RuleCollectionInterface>
      */
     public function getIterator(): Traversable ;
 }
