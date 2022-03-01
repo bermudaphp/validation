75029cfa7ef0c915dec70cdc676f60def6c2d6da
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-01 15:17:38 +0200

    Update YoutubeVideo.php

--- src/rules/YoutubeVideo.php
+++ src/rules/YoutubeVideo.php
@@ -2,26 +2,21 @@
 
 namespace Bermuda\Validation\Rules;
 
-use Bermuda\String\Str;
-
-/**
- * Class YoutubeVideo
- * @package Bermuda\Validation\Rules
- */
-final class YoutubeVideo extends RegExp
+final class YoutubeVideoUrl extends RegEx
 {
-    public const regexp = '~^(?:https?://)?(?:www[.])?(?:youtube[.]com/watch[?]v=|youtu[.]be/)([^&]{11})~x';
+    public const regEx = '~^(?:https?://)?(?:www[.])?(?:youtube[.]com/watch[?]v=|youtu[.]be/)([^&]{11})~x';
 
-    public function __construct()
+    public function __construct(string $message = 'Value must be youtube video url')
     {
-        parent::__construct(self::regexp);
+        parent::__construct(self::regexp, $message);
     }
     
     /**
-     * @param string $regExp
-     * @return $this
+     * @param string $exp
+     * @return self
+     * @throws \RuntimeException
      */
-    public function withExp(string $regExp): self
+    public function withExp(string $exp): self
     {
         throw new \RuntimeException(__METHOD__ . ' is dissable from this class');
     }
