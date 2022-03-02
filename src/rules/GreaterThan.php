a4554befb789276f5a1a9f244b94c12af0765efc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-02 02:39:38 +0200

    Update GreaterThan.php

--- src/rules/GreaterThan.php
+++ src/rules/GreaterThan.php
@@ -2,12 +2,15 @@
 
 namespace Bermuda\Validation\Rules;
 
+/**
+ * @method string|bool validate 
+ */
 final class GreaterThan implements RuleInterface
 {
     use RuleTrait;
     public function __construct(float|int $operand, string $message = 'Value must be greater than :operand')
     {
-        $this->message = $message;
+        $this->messages[] = $message;
         $this->wildcards[':operand'] = $operand;
     }
 
