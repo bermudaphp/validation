19af027e18ce61e52ce1f919ce98eec98f51fb01
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-02 02:49:03 +0200

    Update IsBoolean.php

--- src/rules/IsBoolean.php
+++ src/rules/IsBoolean.php
@@ -4,6 +4,9 @@ namespace Bermuda\Validation\Rules;
 
 use Bermuda\String\StringHelper;
 
+/**
+ * @method string|bool validate 
+ */
 final class IsBoolean implements RuleInterface
 {
     use RuleTrait;
@@ -11,10 +14,10 @@ final class IsBoolean implements RuleInterface
     {
         if ($message == '') {
             $message = $this->strict ? 'Value must be boolean'
-                : 'Value must be boolean or equal any of: \'on\', \'off\', \'true\', \'false\', 0, 1';
+                : 'Value must be boolean or equal any of: \'on\', \'off\', \'true\', \'false\', \'yes\', \'y\', \'n\', \'no\', 0, 1';
         }
 
-        $this->message = $message;
+        $this->messages[] = $message;
     }
 
     protected function doValidate($var): bool
@@ -23,7 +26,7 @@ final class IsBoolean implements RuleInterface
             return is_bool($var);
         }
 
-        return is_bool($var) || (is_string($var) && StringHelper::equals($var, ['on', 'off', '1', '0', 'true', 'false']))
+        return is_bool($var) || (is_string($var) && StringHelper::isBool($var)
             || ($var == 1 || $var == 0);
     }
     
