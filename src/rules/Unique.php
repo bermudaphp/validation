2d54269ea07566b6c3dd4eb7fe0dca036b32aeca
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-03 16:03:57 +0200

    Create Unique.php

---
+++ src/rules/Unique.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Validation;
+
+use Bermuda\Validation\Rules\RuleInterface;
+use Bermuda\Validation\Rules\RuleTrait;
+
+final class Unique implements RuleInterface
+{
+    use RuleTrait;
+    private \Closure $isUnique;
+    public function __construct(callable $isUnique, string $message = 'Must be unique')
+    {
+        $this->messages[] = $message;
+        $this->isUnique = static function($var) use ($isUnique): bool {
+            return $isUnique($var);
+        };
+    }
+
+    public function getName(): string
+    {
+        return 'unique';
+    }
+
+    protected function doValidate($var): bool
+    {
+        return ($this->isUnique)($var);
+    }
+}
