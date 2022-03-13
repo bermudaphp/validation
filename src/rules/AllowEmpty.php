3c899218178c9d97c35fa771dd3873c60055326d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-13 22:25:15 +0200

    Create AllowEmpty.php

---
+++ src/rules/AllowEmpty.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bermuda\Validation\Rules;
+
+final class AllowEmpty implements RuleInterface
+{
+    public function __construct(private RuleInterface $nextRule) {
+    }
+
+    /**
+     * @param $value
+     * @return bool|string|array
+     */
+    public function validate($value): bool|string|array
+    {
+        if (empty($value)) {
+            return true;
+        }
+        
+        return $this->nextRule->validate($value);
+    }
+}
