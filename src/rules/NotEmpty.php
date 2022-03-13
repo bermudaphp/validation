6a4f785b3672b651110e6be7dd7fbfd8b4fc6df8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-13 22:25:46 +0200

    Update NotEmpty.php

--- src/rules/NotEmpty.php
+++ src/rules/NotEmpty.php
@@ -22,29 +22,4 @@ final class NotEmpty implements RuleInterface
     {
         return 'notEmpty';
     }
-
-    /**
-     * @param string $message
-     * @return RuleInterface
-     */
-    public static function reverse(string $message = 'Must be empty'): RuleInterface
-    {
-        return new class($message) implements RuleInterface
-        {
-            use RuleTrait;
-            public function __construct(string $message)
-            {
-                $this->messages[] = $message;
-            }
-            protected function doValidate($var): bool
-            {
-                return empty($var);
-            }
-            
-            public function getName(): string 
-            {
-                return 'empty';
-            }
-        };
-    }
 }
