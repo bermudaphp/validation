96216dbc90b8f9c40d765521d1edc5c1fa108009
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-06-11 12:07:29 +0200

    Update Required.php

--- src/rules/Required.php
+++ src/rules/Required.php
@@ -2,24 +2,26 @@
 
 namespace Bermuda\Validation\Rules;
 
+use Bermuda\Validation\ValidationException;
+
 /**
  * @method string|bool validate
  */
-final class Required implements RuleInterface
+final class Required implements RuleInterface, ValidationDataAwareInterface
 {
-    use RuleTrait;
-    public function __construct(string $columnName)
+    use RuleTrait, ValidationDataTrait;
+    public function __construct(private string $columnName)
     {
         $this->messages[] = "$columnName is required";
     }
 
     protected function doValidate($var): bool
     {
-        if (empty($var) || (is_string($var) && empty(trim($var)))) {
-            return false;
+        if ($this->data === null) {
+            throw new ValidationException('Validation data is null');
         }
         
-        return true;
+        return isset($this->data[$this->columnName]);
     }
 
     public function getName(): string
