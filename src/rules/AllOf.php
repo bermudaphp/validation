70cfc6a187b4d0e7ad7f2cabcb76cc0b81feff35
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-06-11 12:03:58 +0200

    Update AllOf.php

--- src/rules/AllOf.php
+++ src/rules/AllOf.php
@@ -2,10 +2,11 @@
 
 namespace Bermuda\Validation\Rules;
 
-final class AllOf implements RuleCollectionInterface
+final class AllOf implements RuleCollectionInterface, ValidationDataAwareInterface
 {
-    use RuleCollectionTrait;
-
+    use RuleCollectionTrait, ValidationDataTrait;
+    public bool $beforeFirstFailure = false;
+    
     /**
      * @inerhitDoc
      */
@@ -13,15 +14,14 @@ final class AllOf implements RuleCollectionInterface
     {
         $messages = [];
         foreach ($this->rules as $rule) {
+            if ($rule instanceof ValidationDataAwareInterface) $rule->setData($this->data);
             if (($result = $rule->validate($value)) !== true) {
                 $messages[] = $result;
+                if ($this->beforeFirstFailure) break;
             }
         }
         
-        if (($count = count($messages)) == 1) {
-            return $messages[0];
-        }
-
+        if (($count = count($messages)) == 1) return $messages[0];
         return $count > 0 ? $messages : true;
     }
     
