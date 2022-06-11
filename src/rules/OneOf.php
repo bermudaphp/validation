a9d71094f995b1ff66dd11bcb387f3e66e7de9f3
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-06-11 12:28:40 +0200

    Update OneOf.php

--- src/rules/OneOf.php
+++ src/rules/OneOf.php
@@ -4,16 +4,18 @@ namespace Bermuda\Validation\Rules;
 
 use Generator;
 
-final class OneOf implements RuleCollectionInterface
+final class OneOf implements RuleCollectionInterface, ValidationDataAwareInterface
 {
-    use RuleCollectionTrait;
+    use RuleCollectionTrait, ValidationDataTrait;
 
     /**
      * @inheritDoc
      */
     public function validate($value): bool|string|array
     {
+        if ($this->data === null) throw new NullValidationDataException;
         foreach ($this->rules as $rule) {
+            if ($rule instanceof ValidationDataAwareInterface) $rule->setData($this->data);
             if (($result = $rule->validate($value)) === true) {
                 return true;
             }
