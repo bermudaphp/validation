070507471dba356fcf953dafaaa699d472b68c38
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-18 23:20:39 +0200

    Update ValidationException.php

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -4,23 +4,15 @@ namespace Bermuda\Validation;
 
 class ValidationException extends \RuntimeException
 {
-    public function __construct(protected array $errors, protected array $data, public readonly ?Validator $validator = null, int $deep = null)
-    {
-        if ($deep !== null) {
-            $stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $deep)[$deep - 1];
-            $this->file = $stack['file'];
-            $this->line = $stack['line'];
-        }
+    public function __construct(
+        public readonly array $errors, 
+        public readonly array $data, 
+        public readonly ?Validator $validator = null
+    ) {
+        $this->file = ($stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $deep)[2])['file'];
+        $this->line = $stack['line'];
 
         parent::__construct('Validation failed. Number of errors:: ' .count($errors), 422);
-    }
-
-    /**
-     * @return array
-     */
-    public function getErrors(): array
-    {
-        return $this->errors;
     }
 
     /**
@@ -30,13 +22,5 @@ class ValidationException extends \RuntimeException
     public function getErrorsAsJson(string $key = 'errors'): string
     {
         return json_encode([$key => $this->errors]);
-    }
-
-    /**
-     * @return array
-     */
-    public function getValidationData(): array
-    {
-        return $this->data;
     }
 }
