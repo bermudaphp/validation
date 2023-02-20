6a3a02f0588a66f762ddd6cde20c584d2d6beeaf
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-20 13:22:45 +0200

    Update ValidatorInterface.php

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -6,8 +6,8 @@ interface ValidatorInterface
 {
     /**
      * @param array $data
-     * Throws an ValidationException if validation fails
      * @throws ValidationException
+     * Throws an ValidationException if validation fails
      */
     public function validate(array $data): void ;
 }
